8701078242374ddd490e84367054325685bc4179
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2023-02-27 20:59:06 +0530

    2

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -19,6 +19,7 @@ class _HomescreenState extends State<Homescreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.cyan,
           elevation: 0,
           title: Text("Table App", style: TextStyle(letterSpacing: 2)),
           actions: [
@@ -36,20 +37,33 @@ class _HomescreenState extends State<Homescreen> {
             key: formkey,
             child: Column(
               children: [
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: txtInput,
-                  validator: (value) {
-                    if (inputNumber < 0 || check == false) {
-                      return "enter valid input !";
-                    } else {
-                      null;
-                    }
-                  },
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1.5, color: Colors.black),
-                  )),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onTapOutside: (event) => false,
+                    keyboardType: TextInputType.number,
+                    controller: txtInput,
+                    validator: (value) {
+                      if (inputNumber < 0 || check == false) {
+                        return "enter valid number !";
+                      } else {
+                        null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Table no.",
+                         labelStyle: TextStyle( color: Colors.cyan),
+                        focusedBorder:OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.cyan,width: 2)
+                        ),
+                        disabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2,color: Colors.cyan),
+                        ),
+                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan,width: 1)),
+                        border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1.5, color: Colors.cyan),
+                    )),
+                  ),
                 ),
                 SizedBox(
                   height: 10,
@@ -67,7 +81,8 @@ class _HomescreenState extends State<Homescreen> {
                         formkey.currentState!.deactivate();
                       });
                     },
-                    child: Text("Submit")),
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.cyan)),
+                    child: Text("Submit"),),
                 SizedBox(
                   height: 10,
                 ),
@@ -78,16 +93,18 @@ class _HomescreenState extends State<Homescreen> {
                       margin:  EdgeInsets.all(10),
                       width: double.infinity,
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black,width: 1.5),
+                        border: Border.all(color: Colors.cyan.shade700,width: 2),
                         borderRadius: BorderRadius.circular(20),
-                        color: Colors.blue.shade300,
+                        color: Colors.cyan.shade200,
                       ),
                       alignment: Alignment.center,
                       child: Center(
                         child: ListView.builder(
 
-                          itemBuilder: (context, index) => Text(
-                              "${inputNumber} * ${index + 1} = ${inputNumber * (index + 1)}",style: TextStyle(fontSize: 30)),
+                          itemBuilder: (context, index) => Center(
+                            child: Text(
+                                "${inputNumber} * ${index + 1} = ${inputNumber * (index + 1)}",style: TextStyle(fontSize: 30)),
+                          ),
                           itemCount: tableAns.length,
                           shrinkWrap: true,
                         ),
